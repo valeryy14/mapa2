e451615915b14128461ecadcc074262d65b8c1b6
iscalejandrogr <i.s.c.alejandro.gr@gmail.com>
2015-05-29 19:12:36 -0500

    comentario1

--- conexion.php
+++ conexion.php
@@ -1,9 +1,13 @@
 <?php 
-$servidor="localhost";
+/*$servidor="localhost";
 $usuario="root";
 $clave="alex";
-$BaseDatos="servicioslibres";
+$BaseDatos="servicioslibres";*/
 
+$servidor="sql206.260mb.net";
+$usuario="n260m_14426545";
+$clave="drag0n";
+$BaseDatos="n260m_14426545_servicios";
 $conexion=mysql_connect($servidor,$usuario,$clave)or die("Error al establecer la conexión: ".mysql_error());
 
 mysql_select_db($BaseDatos,$conexion);
